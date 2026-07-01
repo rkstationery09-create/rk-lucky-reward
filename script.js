cce83e9a3429af933138d57b3dbdc0ebddda27ff
rkstationery09-create <r.k.stationery09@gmail.com>
2026-07-01 16:40:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,19 +11,31 @@ return;
 let offers = [];
 
 if(amount >= 50 && amount < 100){
-offers = ["🎁 ₹5 Pen","🎁 ₹5 Pencil","🎁 ₹5 Eraser","🎁 ₹5 Sharpener","🎁 ₹5 Scale"];
+offers = ["🎁 ₹5 Pen","🎁 ₹5 Eraser","🎁 ₹5 Pencil","🎁 ₹5 Sharpener","🎁 ₹5 Scale","🎁 ₹5 Glue"];
 }
+
 else if(amount >= 100 && amount < 200){
-offers = ["🎁 ₹10 Pen","🎁 ₹10 Sketch Color","🎁 ₹10 Pencil","🎁 ₹10 Fevistick"];
+offers = ["🎁 ₹10 Pen","🎁 ₹10 Fevicol","🎁 ₹10 Stapler Pin","🎁 ₹15 Sketch Color","🎁 ₹15 Glue Stick","🎁 ₹10 Wax Color","🎁 ₹10 Pencil Color","🎁 ₹10 Water Color","🎁 ₹10 Pen Pencil Combo"];
 }
+
 else if(amount >= 200 && amount < 300){
-offers = ["🎁 ₹20 Diary","🎁 ₹20 Color Set","🎁 ₹20 Kit"];
+offers = ["🎁 ₹20 Water Color","🎁 ₹25 Fevicol","🎁 ₹20 Pocket Diary","🎁 ₹25 Wax Color","🎁 ₹30 Fevistick","🎁 ₹20 Sharpener","🎁 ₹25 Highlighter"];
 }
+
+else if(amount >= 300 && amount < 400){
+offers = ["🎁 ₹30 Notebook","🎁 ₹35 Sketch Color","🎁 ₹30 Water Bottle","🎁 ₹30 Water Color","🎁 ₹30 Whitener","🎁 ₹30 Clay","🎁 ₹30 Pouch Box"];
+}
+
+else if(amount >= 400 && amount < 500){
+offers = ["🎁 ₹48 Natraj Pencil Box","🎁 ₹40 Fevistick","🎁 ₹40 Register Small","🎁 ₹40 Glue Bottle","🎁 🎁 4 Pen Combo"];
+}
+
 else{
-offers = ["🎁 ₹30 Gift","🎁 🎉 Surprise Box","🎁 ₹30 Premium Kit"];
+offers = ["🎁 ₹60 Apsara Pencil Box","🎁 ₹50 Geometry Box","🎁 ₹50 Tiffin Box","🎁 ₹50 Fevistick"];
 }
 
 let random = Math.floor(Math.random() * offers.length);
 
-result.innerHTML = "💰 ₹" + amount + " → " + offers[random];
+result.innerHTML =
+"💰 ₹" + amount + " → " + offers[random];
 }
