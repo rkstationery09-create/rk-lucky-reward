fb085f74f927407e1732bf815c630763773eb303
rkstationery09-create <r.k.stationery09@gmail.com>
2026-07-01 15:28:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Script File
+function getOffer(){
+
+let amount = document.getElementById("amount").value;
+
+if(amount < 100){
+  document.getElementById("result").innerHTML = "❌ Minimum ₹100 required";
+  return;
+}
+
+let discount = amount / 10;
+
+document.getElementById("result").innerHTML =
+"🎁 You got ₹" + discount + " OFF";
+}
